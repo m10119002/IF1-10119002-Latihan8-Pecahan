05f442a76dc361b8b4f69009a3d013fc91aa691d
m10119002 <firman.10119002@mahasiswa.unikom.ac.id>
2020-10-21 01:43:26 +0700

    21 Rabu 2020

---
+++ src/m10119002/pbo/latihan8/Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m10119002.pbo.latihan8;
+
+/**
+ *
+ * @author
+ * NAMA     : Firman Sahita
+ * KELAS    : PBO1
+ * NIM      : 10119002
+ * Deskripsi Program : Program ini berisi program pecahan
+ * class
+ * 
+ */
+public class Pecahan {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+}
